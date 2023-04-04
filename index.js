5e419af11050fb3306df0b6b26a06265a1739238
IT20618186 <it20618186@my.sliit.lk>
2023-04-04 14:32:05 +0530

    Builded Application Server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Running Application Successfully..!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database Successfully Connected"))
+  .catch((error) => console.log(error));
+
+app.use("/auth", require("./routes/UserLogin"));
+
+const port = process.env.PORT || 8100;
+
+app.listen(port, () => {
+  console.log(`Server Running on Port - ${port}`);
+});
