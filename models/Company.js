cdf7d927d5f3eca50b87a0df16a5c7a1cbb01897
HashiFerdo <hashifernando1999@gmail.com>
2023-05-19 05:45:36 +0530

    company requirement  backend part.

--- models/Company.js
+++ models/Company.js
@@ -2,15 +2,30 @@ const mongoose = require('mongoose');
 
 const companySchema = new mongoose.Schema({
 
-    Company_ID:{
+    C_req_ID:{
         type :String,
         required:true
+    },
+    C_name:{
+        type :String,
+        required :true
+    },
+    C_location:{
+        type :String,
+        required :true
+    },
+    requirement:{
+        type :String,
+        required :true
+    },
+    Cinnamon_grade:{
+        type :String
+    },
+    Date:{
+        type :Date,
+        required :true
     }
-
-
-
-
-
-
-
 })
+
+
+module.exports = mongoose.model('company_requirement',companySchema)
