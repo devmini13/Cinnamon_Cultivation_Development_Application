40c17d6531160da6f30b6a3aa46cb3229901ca4c
HashiFerdo <hashifernando1999@gmail.com>
2023-05-19 07:01:08 +0530

    Delete record.

--- routes/Company_req.js
+++ routes/Company_req.js
@@ -21,3 +21,53 @@ router.post('/company/save', (req, res)=>{
     });
 });
 
+//Get company requirements
+
+router.get('/company/view', (req, res)=>{
+    Company_req.find().exec((err,company_reqs)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:true,
+            existingCompany_req:company_reqs
+        });
+    });
+});
+
+
+//Update company requirements
+router.patch('/company/update/:id',(req,res)=>{
+    Company_req.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,Company_req)=>{
+            if(err){
+                return res.status(400).json({error:err})
+            }
+            return res.status(200).json({
+                success:"Updated company requirement successfully" 
+            });
+        }
+    );
+})
+
+//Delete company requirement
+router.delete('/company/delete/:id',(req,res)=>{
+    Company_req.findByIdAndRemove(req.params.id).exec((err,deleteCompany_req)=>{
+        
+        if(err) return res.status(400).json({
+            message:"deleted unsuccessful",err
+        });
+
+        return res.json({
+            message:"deleted successful",deleteCompany_req
+        });
+
+    });
+}); 
+
