6aa2068bd7c1350141ab858ec4053bec8d09ae87
devmini13 <it20271282@my.sliit.lk>
2023-05-19 18:00:14 +0530

    Save stateOwner details by POST method

---
+++ routes/StateOwner.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const StateOwner = require('../models/StateOwner');
+
+//Save stateOwner details
+router.post('/stateOwner/save',(req,res)=>{
+  let newStateOwner = new StateOwner(req.body);
+
+  newStateOwner.save((err) =>{
+    if(err){
+      return res.status(400).json({
+        error:err
+      });
+    }
+    return res.status(200).json({
+      success:"Saves Successfully"
+    });
+});
+
+});
+
+
+
+
+
+
+module.exports = router;
